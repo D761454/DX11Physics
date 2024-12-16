3c08b32dd4393c0beb60c816cb6799bfaa2523da
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-12-16 15:12:27 +0000

    get and set methods for acceleration

--- DX11PhysicsFramework/DX11PhysicsFramework.cpp
+++ DX11PhysicsFramework/DX11PhysicsFramework.cpp
@@ -543,7 +543,9 @@ HRESULT DX11PhysicsFramework::InitRunTimeData()
 	gameObject->GetAppearance()->SetTextureRV(_StoneTextureRV);
 	_gameObjects.push_back(gameObject);
 
+	_gameObjects[1]->GetPhysicsModel()->SetVelocity(Vector3(0, 1, 0));
 	_gameObjects[2]->GetPhysicsModel()->SetVelocity(Vector3(0,1,0));
+	_gameObjects[2]->GetPhysicsModel()->SetAcceleration(Vector3(0, 1, 0));
 
 	_timer = new Timer();
 
--- DX11PhysicsFramework/PhysicsModel.h
+++ DX11PhysicsFramework/PhysicsModel.h
@@ -9,11 +9,13 @@ private:
 protected:
 	Transform* _transform;
 	Vector3 _velocity;
-	Vector3 _acceleration = { 0.0f, 1.0f, 0.0f };
+	Vector3 _acceleration;
 
 public:
 	Vector3 GetVelocity() const { return _velocity; }
 	void SetVelocity(Vector3 velocity) { _velocity = velocity; }
+	Vector3 GetAcceleration() const { return _acceleration; }
+	void SetAcceleration(Vector3 acceleration) { _acceleration = acceleration; }
 
 	PhysicsModel(Transform* transform);
 
